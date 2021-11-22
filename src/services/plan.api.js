6146893ff38da5ed586376b57638ee60b314cee7
rabbithay <thay.csb@gmail.com>
2021-11-22 17:34:48 -0300

    fix: connect to heroku

--- src/services/plan.api.js
+++ src/services/plan.api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:4000'
-  : 'https:/';
+  : 'https://gratiboxx.herokuapp.com';
 
 export function getProductsList(config) {
   return axios.get(`${BASE_URL}/products`, config);
--- src/services/user.api.js
+++ src/services/user.api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:4000'
-  : 'https:/';
+  : 'https://gratiboxx.herokuapp.com';
 
 export function createNewUser(body) {
   return axios.post(`${BASE_URL}/register`, body);
